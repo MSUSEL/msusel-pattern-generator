7af902741187cf9d3789b27f37854cef86cd5a5d
Isaac Griffith <isaacgriffith@gmail.com>
2021-05-03 16:29:05 -0600

    updated

--- src/main/groovy/edu/montana/gsoc/msusel/pattern/gen/generators/java/JavaTypeGenerator.groovy
+++ src/main/groovy/edu/montana/gsoc/msusel/pattern/gen/generators/java/JavaTypeGenerator.groovy
@@ -93,6 +93,7 @@ class JavaTypeGenerator extends TypeGenerator {
         ${typeComment()}
          */
         ${createTypeDef(kind, type, name)} {${getContent(type)}
+
         }
         """
     }
